c60831f619c9d9ec00ea48fcb4d0b15a6ce8cf2d
Nipun Lamba <nipun00123@gmail.com>
2021-01-09 22:20:57 -0500

    picture functionality fully added

--- frontend/components/AddIngredients.js
+++ frontend/components/AddIngredients.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { RNCamera } from 'react-native-camera'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {
@@ -7,14 +7,52 @@ import {
   ScrollView,
   View,
   Alert,
+  Image,
   Text,
   StatusBar,
+  Dimensions,
   TouchableOpacity,
 } from 'react-native';
 
 const AddIngredients = () => {
-  return (
-    <View style={styles.container}>
+
+  const [path, setPath] = useState(null);
+  const [visibleStatusBar, setVisibleStatusBar] = useState(false);
+
+  takePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      setPath(data.uri);
+      setVisibleStatusBar(true);
+      console.log(data.uri);
+    }
+  } 
+
+  renderImage = () => {
+    return (
+      <View>
+        <Image
+          source={{uri : path}}
+          style={styles.preview}
+        />
+        <Text
+          style={styles.cancel}
+          onPress={() => {
+            setPath(null)
+            console.log('clicked');
+            setVisibleStatusBar(false);
+          }}
+        >
+        <Ionicons size={32} style={styles.snap} name="close-outline"/>
+        </Text>
+      </View>
+    )
+  }
+
+  renderCamera = () => {
+    return (
+      <View style={styles.container}>
       <RNCamera
         style={styles.preview}
         ref={ref => {
@@ -23,45 +61,56 @@ const AddIngredients = () => {
         captureAudio={false}
         type={RNCamera.Constants.Type.back}
       />
-      <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
-        <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+      <View style={{flex: 0, flexDirection: 'column', justifyContent: 'center'}}>
+        <TouchableOpacity onPress={takePicture.bind(this)} style={styles.capture}>
           <View><Ionicons size={64} style={styles.snap} name="ellipse-outline"/></View>
         </TouchableOpacity>
       </View>
     </View>
+    )
+  }
+
+
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <StatusBar hidden={visibleStatusBar} />
+      </View>
+      {path ? renderImage() : renderCamera()}
+    </View>
   );
 };
-
-takePicture = async () => {
-    if (this.camera) {
-      const options = { quality: 0.5, base64: true };
-      const data = await this.camera.takePictureAsync(options);
-      console.log(data.uri); 
-    }
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
+    alignItems: 'center',
+
   },
   preview: {
     flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
   },
   snap: {
     color: 'white'
   },
   capture: {
     flex: 0,
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
     alignSelf: 'center',
-    margin: 20,
   },
+  cancel : {
+    position: 'absolute',
+    left: 10,
+    top: 40,
+    backgroundColor: 'transparent',
+    color: '#FFF',
+    fontWeight: '600',
+    fontSize: 17,
+  }
 });
 
 export default AddIngredients;
