57e1c234a0c2e99d2bfc800f3da418efaf268ae3
Nipun Lamba <nipun00123@gmail.com>
2021-01-14 12:13:21 -0500

    thresholding done

--- backend/main.go
+++ backend/main.go
@@ -6,11 +6,15 @@ import (
 )
 
 func main() {
-	window := gocv.NewWindow("Receipt")
+	window := gocv.NewWindow("Original")
 
-	img := gocv.IMRead("/Use/nipunlamba/iCHEF/assets/IMG_2763.jpg", 0)
-	gocv.Threshold(img, &img, 210, 255, gocv.ThresholdBinary)
+	img := gocv.IMRead("/Users/nipunlamba/iCHEF/assets/IMG_2763.jpg", 0)
+
+	gocv.Threshold(img, &img, 127, 255, gocv.ThresholdBinary)
+	// gocv.AdaptiveThreshold(img2, &img2, 255, gocv.AdaptiveThresholdMean, gocv.ThresholdBinary, 11, 2)
+	// gocv.AdaptiveThreshold(img3, &img3, 255, gocv.AdaptiveThresholdGaussian, gocv.ThresholdBinary, 11, 2)
+
 	window.IMShow(img)
-	window.WaitKey(5000)
+	window.WaitKey(10000)
 
 }
