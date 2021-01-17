867b6f35d2f372b135b6314b88fc839df7d99f84
Nipun Lamba <nipun00123@gmail.com>
2021-01-17 14:44:32 -0500

    regexp

--- backend/main.go
+++ backend/main.go
@@ -1,20 +1,50 @@
-// PREPROCESSING // adopt adaptive
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/otiai10/gosseract"
 	"gocv.io/x/gocv"
 )
 
 func main() {
-	window := gocv.NewWindow("Original")
 
-	img := gocv.IMRead("/Users/nipunlamba/iCHEF/assets/IMG_2763.jpg", 0)
+	img := gocv.IMRead("/Users/nipunlamba/iCHEF/assets/IMG_2783.png", 0)
+	// window := gocv.NewWindow("org")
 
-	gocv.Threshold(img, &img, 127, 255, gocv.ThresholdBinary)
-	// gocv.AdaptiveThreshold(img2, &img2, 255, gocv.AdaptiveThresholdMean, gocv.ThresholdBinary, 11, 2)
-	// gocv.AdaptiveThreshold(img3, &img3, 255, gocv.AdaptiveThresholdGaussian, gocv.ThresholdBinary, 11, 2)
+	// PREPROCESSING //
+	//gocv.MedianBlur(img, &img, 5)
+	//gocv.Threshold(img, &img, 127, 255, gocv.ThresholdBinary)
+	//ocv.AdaptiveThreshold(img, &img, 255, gocv.AdaptiveThresholdMean, gocv.ThresholdBinary, 11, 2)
+	//gocv.AdaptiveThreshold(img, &img, 255, gocv.AdaptiveThresholdGaussian, gocv.ThresholdBinary, 11, 2)
 
-	window.IMShow(img)
-	window.WaitKey(10000)
+	// Text Detection
+	client := gosseract.NewClient()
+	defer client.Close()
+	ImgBuffer, _ := gocv.IMEncode(gocv.PNGFileExt, img)
+	client.SetImageFromBytes(ImgBuffer)
+
+	text, _ := client.Text()
+	fmt.Println(text)
+	scanner := bufio.NewScanner(strings.NewReader(text))
+	for scanner.Scan() {
+		line := scanner.Text()
+		itemValue := regexp.MustCompile(`\$[0-9]*\.[0-9]{2}`)
+
+		subtotal := regexp.MustCompile(`^(?is)sub\s?total.*[0-9]{2}$`)
+
+		itemVal := itemValue.FindString(line)
+		subtotalVal := subtotal.FindString(line)
+
+		if subtotalVal == "" {
+			fmt.Println(itemVal)
+		} else {
+			fmt.Println(subtotalVal)
+		}
+
+	}
 
 }
